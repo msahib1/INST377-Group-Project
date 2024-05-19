6fb941055944b05b59ab6c66e51cfd5258345f98
msahib1 <98369937+msahib1@users.noreply.github.com>
2024-05-19 17:42:54 -0400

    Updated index.js

--- index.js
+++ index.js
@@ -62,3 +62,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+const { createClient } = require('@supabase/supabase-js');
